829fd32febaa612620e265d243a235c9506d5420
MilanKovacic <milankovacicmk@outlook.com>
2016-12-17 16:50:24 +0100

    Initial commit

---
+++ GiveMe/GiveMe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GiveMe.Startup))]
+namespace GiveMe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
